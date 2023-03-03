2f31a76bdc8a59e6eba4e3ab393e9fa06fccfd7b
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-03 20:38:01 -0300

    Create add 3 new files

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = require('./config/routes.js')
+const express = require('express')
+const app = express()
+app.use('./app/css/', express.static('/app/css/'))
+app.use('./app/js/', express.static('/app/js/'))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/app/view/index.html')
+})
+
+app.listen(3000, (err, done)=>{
+    if(!err){
+        console.log({ message: 'sucess' })
+    }
+})
